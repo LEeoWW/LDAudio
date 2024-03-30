8781c773be1ffbd39bfebb0868e06e7c6f48f8c0
LEeOW <104145371+LEeoWW@users.noreply.github.com>
2024-03-30 12:04:16 +0100

    Update script.js

--- lightdropWeb/galery/script.js
+++ lightdropWeb/galery/script.js
@@ -12,3 +12,26 @@ function showSlides(n) {
     slides.forEach(slide => slide.style.display = "none");
     slides[slideIndex].style.display = "block";
 }
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    document.getElementById('scrollToTopBtn').style.display = 'block';
+}
+
+var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+// 当页面滚动时，显示或隐藏按钮
+window.addEventListener('scroll', function() {
+var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+if (window.scrollY > 100) {
+scrollToTopBtn.style.display = 'block';
+} else {
+scrollToTopBtn.style.display = 'none';
+}
+});
+
+// 点击按钮回到页面顶部
+document.getElementById('scrollToTopBtn').addEventListener('click', function() {
+window.scrollTo({
+top: 0,
+behavior: 'smooth'
+});
+});
